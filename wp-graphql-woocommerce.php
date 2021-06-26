a9cd16ac073381a6d5985ec24c895ed2748c5724
Paweł Kapała <bylek77@gmail.com>
2021-06-26 21:12:24 +0200

    Add pagination to products

--- wp-graphql-woocommerce.php
+++ wp-graphql-woocommerce.php
@@ -100,3 +100,39 @@ function woographql_init() {
 	return false;
 }
 add_action( 'graphql_init', 'woographql_init' );
+
+
+/**
+ * Add offset pagination to products.
+ */
+function theme_prefix_add_offset_pagination_to_products() {
+	register_graphql_field(
+		'RootQueryToProductConnectionWhereArgs',
+		'offsetPagination',
+		array(
+			'type'        => 'OffsetPagination',
+			'description' => 'Paginate content nodes with offsets',
+		)
+	);
+}
+add_action( 'graphql_register_types', 'theme_prefix_add_offset_pagination_to_products' );
+/**
+ * Add offset pagination to products.
+ *
+ * @param array $query_args query args.
+ * @param array $where_args where query args.
+ */
+function theme_prefix_filter_map_offset_to_wp_query_args(
+	array $query_args,
+	array $where_args
+) {
+	if ( isset( $where_args['offsetPagination']['offset'] ) ) {
+		$query_args['offset'] = $where_args['offsetPagination']['offset'];
+	}
+	if ( isset( $where_args['offsetPagination']['size'] ) ) {
+		$query_args['posts_per_page'] =
+			intval( $where_args['offsetPagination']['size'] ) + 1;
+	}
+	return $query_args;
+}
+add_filter( 'graphql_map_input_fields_to_product_query', 'theme_prefix_filter_map_offset_to_wp_query_args', 10, 2 );
